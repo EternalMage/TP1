d1bd6600c1b6aaf567fd0b3e68c463fe6455bd5c
Jonathan G.-R <jonathan.g-r@outlook.com>
2019-10-05 19:21:03 -0400

    Author(s) add remove.

--- TP2/javascripts/publications.js
+++ TP2/javascripts/publications.js
@@ -1,4 +1,19 @@
 $(document).ready(() => {
     const setTrashEvent = () => $('.fa-trash-o').click((e) => $(e.target).parent().parent().parent().remove())
     setTrashEvent()
+
+    const setMinus = () => $('.fa-minus').click((e) => $(e.target).parent().remove())
+    const setAddAuthor = () => {
+        $('.fa-plus').click((e) => {
+            const newAuthor = `
+              <span id="author">
+                <input type="text" name="authors[]" placeholder="Auteurs" value="Auteur test" minlength="5" required="">
+                <i class="fa fa-minus fa-3x"></i>
+              </span>
+            `
+            $('.author-input').append(newAuthor)
+            setMinus()
+        })
+    }
+    setAddAuthor()
 })

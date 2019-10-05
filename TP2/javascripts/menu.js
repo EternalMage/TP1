ed34c2e4d8593d7a5614e0cace3c57be1b6166b8
Jonathan G.-R <jonathan.g-r@outlook.com>
2019-10-05 18:00:27 -0400

    Gestion du menu.

---
+++ TP2/javascripts/menu.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+    const index = { page: '', id: 'home-link' }
+    const projectsLink = 'projects-link'
+    const pages = [{ page: 'project-maillouxmaster', id: projectsLink }, { page: 'project-nbourgoinmaster', id: projectsLink }, { page: 'projects', id: 'projects-link' }, { page: 'team', id: 'team-link' }, { page: 'publications', id: 'publications-link' }]
+
+    const menuActive = (page, search) => {
+        return (page.indexOf(search) === -1) ? false : true
+    }
+
+    const activateMenu = (id) => {
+        $('#' + id).addClass('active')
+    }
+
+    const updateMenu = (pages) => {
+        const url = $(document)[0].URL.split('/').pop()
+        if (url === index.page) {
+            activateMenu(index.id)
+        } else {
+            pages.map(p => {
+                if (menuActive(p.page, url)) {
+                    activateMenu(p.id)
+                }
+            })
+        }
+    }
+
+    updateMenu(pages)
+});
--- TP2/javascripts/utils.js
+++ TP2/javascripts/utils.js
@@ -8,11 +8,11 @@
  * genererCompteur() // Renvoie 2
  * genererCompteur() // Renvoie 3
  */
-const genererCompteur = function (x) {
-  return function () {
-    x = x + 1
-    return x
-  }
+const genererCompteur = function(x) {
+    return function() {
+        x = x + 1
+        return x
+    }
 }
 
 /**
@@ -28,11 +28,11 @@ const genererCompteur = function (x) {
  * @returns {Object<string, number>}
  */
 const charCounts = function(str) {
-  let t = {}
-  // The "g" flag indicates that the regular expression should be tested against all possible matches in a string.
-  let strArray = str.toLowerCase().match(/[A-Za-z]/g)
-  strArray.map(s => t[s] = (t[s] !== undefined)? t[s] + 1 : 1)
-  return t
+    let t = {}
+        // The "g" flag indicates that the regular expression should be tested against all possible matches in a string.
+    let strArray = str.toLowerCase().match(/[A-Za-z]/g)
+    strArray.map(s => t[s] = (t[s] !== undefined) ? t[s] + 1 : 1)
+    return t
 }
 
 /**
@@ -43,14 +43,14 @@ const charCounts = function(str) {
  * @param {Date} date - Objet de Date
  * @returns {number} Nombre de jours jusqu'à Noël prochain.
  */
-const daysToChristmas = function(date){
-  var christmasDate=new Date(date.getFullYear(), 11, 25); // 11 for december
-  // christmasDate - date given = number of milliseconds before Christmas
-  if (christmasDate-date < 0){
-    christmasDate=new Date(date.getFullYear() + 1, 11, 25);
-  }
-  // convertir milliseconds result to number of days 1000mil x 60s x 60min x 24h
-  return Math.ceil((christmasDate-date)/(1000*60*60*24))
+const daysToChristmas = function(date) {
+    var christmasDate = new Date(date.getFullYear(), 11, 25); // 11 for december
+    // christmasDate - date given = number of milliseconds before Christmas
+    if (christmasDate - date < 0) {
+        christmasDate = new Date(date.getFullYear() + 1, 11, 25);
+    }
+    // convertir milliseconds result to number of days 1000mil x 60s x 60min x 24h
+    return Math.ceil((christmasDate - date) / (1000 * 60 * 60 * 24))
 }
 
 /**
@@ -65,12 +65,12 @@ const daysToChristmas = function(date){
  * @returns {Array} Tableau sans éléments dupliqués
  */
 const distinct = function(arr) {
-  // reduce: takes accumulator a (which starts at []), parse through array (current value b), execute func
-  var distinctArr = arr.reduce(function(a,b){
-    if (a.indexOf(b) < 0 ) a.push(b)
-    return a
-  },[])
-  return distinctArr
+    // reduce: takes accumulator a (which starts at []), parse through array (current value b), execute func
+    var distinctArr = arr.reduce(function(a, b) {
+        if (a.indexOf(b) < 0) a.push(b)
+        return a
+    }, [])
+    return distinctArr
 }
 
 /**
@@ -85,10 +85,10 @@ const distinct = function(arr) {
  * @param {Object} obj2 - Deuxième objet
  * @returns {Array} Tableau qui contient les cléfs partagées entre deux objets
  */
-const commonKeys = function(obj1, obj2){
-  let a = []
-  a = Object.keys(obj1).filter(value => Object.keys(obj2).indexOf(value) !== -1)
-  return a
+const commonKeys = function(obj1, obj2) {
+    let a = []
+    a = Object.keys(obj1).filter(value => Object.keys(obj2).indexOf(value) !== -1)
+    return a
 }
 
 /**
@@ -101,48 +101,48 @@ const commonKeys = function(obj1, obj2){
  * @param {Boolean} asc - True si on trie en ordre croissant. False pour décroissant
  * @returns {Array} Tableau trié
  */
-const sortByAuthorAndTitle = function(arr, asc){
-  asc === undefined ? asc=true : null
-  let sortAsc = function( a, b ) {
-    // compare authors
-    if ( a.author < b.author ){
-      return -1;
-    }
-    if ( a.author > b.author ){
-      return 1;
-    }
-    
-    // else compare titles
-    if (a.title < b.title) { 
-        return -1;
-    } else if (a.title > b.title) {
-        return 1
-    } else {
-        return 0;
-    }
-  }
+const sortByAuthorAndTitle = function(arr, asc) {
+    asc === undefined ? asc = true : null
+    let sortAsc = function(a, b) {
+        // compare authors
+        if (a.author < b.author) {
+            return -1;
+        }
+        if (a.author > b.author) {
+            return 1;
+        }
 
-  let sortDsc = function( a, b ) {
-    // compare authors
-    if ( b.author < a.author ){
-      return -1;
+        // else compare titles
+        if (a.title < b.title) {
+            return -1;
+        } else if (a.title > b.title) {
+            return 1
+        } else {
+            return 0;
+        }
     }
-    if ( b.author > a.author ){
-      return 1;
-    }
-    
-    // else compare titles
-    if (b.title < a.title) { 
-        return -1;
-    } else if (b.title > a.title) {
-        return 1
-    } else {
-        return 0;
-    }
-  }
 
-  asc ? arr.sort(sortAsc) : arr.sort(sortDsc)
-  return arr
+    let sortDsc = function(a, b) {
+        // compare authors
+        if (b.author < a.author) {
+            return -1;
+        }
+        if (b.author > a.author) {
+            return 1;
+        }
+
+        // else compare titles
+        if (b.title < a.title) {
+            return -1;
+        } else if (b.title > a.title) {
+            return 1
+        } else {
+            return 0;
+        }
+    }
+
+    asc ? arr.sort(sortAsc) : arr.sort(sortDsc)
+    return arr
 }
 
 /**
@@ -162,14 +162,14 @@ const sortByAuthorAndTitle = function(arr, asc){
  * @param {any} z - 3e paramètre
  * @returns {Function} Fonction currifiée
  */
-const curry3 = function (op) {
-  return (x) => {
-    return (y) => {
-      return (z) => {
-          return op(x, y, z)
-      }
+const curry3 = function(op) {
+    return (x) => {
+        return (y) => {
+            return (z) => {
+                return op(x, y, z)
+            }
+        }
     }
-  }
 }
 
 /**
@@ -189,7 +189,7 @@ const curry3 = function (op) {
  * @returns {Array}
  */
 function map(arr, callback) {
-  return arr.map(callback)
+    return arr.map(callback)
 }
 
 /**
@@ -209,8 +209,8 @@ function map(arr, callback) {
  * @returns {Array}
  */
 function find(arr, predicate) {
-  let value = arr.find(predicate)
-  return value !== undefined ? value : null
+    let value = arr.find(predicate)
+    return value !== undefined ? value : null
 }
 
 /**
@@ -245,7 +245,7 @@ function find(arr, predicate) {
  * @returns {S} Accumulateur
  */
 function fold(arr, init, op) {
-  return undefined
+    return undefined
 }
 
 /**
@@ -261,23 +261,23 @@ function fold(arr, init, op) {
  * e.toString() // Renvoie 'Employee name=Konstantinos,salary=50000
  */
 class Employee {
-  constructor(id, name, salary) {
-    this._id = id
-    this._name = name
-    this._salary = salary
-  }
+    constructor(id, name, salary) {
+        this._id = id
+        this._name = name
+        this._salary = salary
+    }
 
-  // setters & getters
-  set name(name) { this._name = name; }
-  get name() { return this._name; }
-  /*set id(id) { this._id = id; }*/
-  get id() { return this._id; }
-  /*set salary(salary) { this._salary = salary; }*/
-  get salary() { return this._salary; }
+    // setters & getters
+    set name(name) { this._name = name; }
+    get name() { return this._name; }
+        /*set id(id) { this._id = id; }*/
+    get id() { return this._id; }
+        /*set salary(salary) { this._salary = salary; }*/
+    get salary() { return this._salary; }
 
-  toString() {
-    return 'Employee name=' + this._name + ',salary=' + this._salary
-  }
+    toString() {
+        return 'Employee name=' + this._name + ',salary=' + this._salary
+    }
 }
 
 /**
@@ -292,33 +292,35 @@ class Employee {
  * e.toString() // Renvoie 'Employee name=Konstantinos,salary=50000,bonus=10
  */
 class Chercheur extends Employee {
-  constructor(id, name, salary, bonus) {
-    super(id, name, salary)
-    this._bonus = bonus
-  }
+    constructor(id, name, salary, bonus) {
+        super(id, name, salary)
+        this._bonus = bonus
+    }
 
-  get salary() {
-    return this._salary + (this._salary * this._bonus / 100)
-  }
-  toString() {
-    return 'Employee name=' + this._name + ',salary=' + this._salary + ',bonus=' + this._bonus
-  }
+    get salary() {
+        return this._salary + (this._salary * this._bonus / 100)
+    }
+    toString() {
+        return 'Employee name=' + this._name + ',salary=' + this._salary + ',bonus=' + this._bonus
+    }
 }
 
 //const c = new Chercheur(1, 'Konstantinos', 50000, 10)
 //console.log(c.toString()) // Renvoie 'Employee name=Konstantinos,salary=50000,bonus=10
 
 export {
-  genererCompteur,
-  charCounts,
-  daysToChristmas,
-  distinct,
-  commonKeys,
-  sortByAuthorAndTitle,
-  curry3,
-  map,
-  find,
-  fold,
-  Employee,
-  Chercheur
+    genererCompteur,
+    charCounts,
+    daysToChristmas,
+    distinct,
+    commonKeys,
+    sortByAuthorAndTitle,
+    curry3,
+    map,
+    find,
+    fold,
+    Employee,
+    Chercheur
 }
+
+alert('test')

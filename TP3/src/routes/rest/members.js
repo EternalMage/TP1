57fd2023443dcdcf3ee16dbe5ebbc3ec5652f73f
Thoai-Vi Le <thoai-vi.le@polymtl.ca>
2019-11-02 15:11:12 -0400

    implementation routes/rest/members.js DONE

--- TP3/src/routes/rest/members.js
+++ TP3/src/routes/rest/members.js
@@ -4,6 +4,19 @@ module.exports = serviceTeam => {
   const router = express.Router()
 
   // À COMPLÉTER
+  router.get('/', (req, res, next) => {
+	serviceTeam.getTeamMembers((err, teamMembers) => {
+		if (err) {
+			if (req.app.locals.t === undefined || req.app.locals.t['ERRORS'] === undefined || req.app.locals.t['ERRORS']['MEMBERS_ERROR'] === undefined) {
+				res.status(500).json({ 'errors': [err.message] });
+			  } else {
+				  res.status(500).json({ 'errors': [req.app.locals.t['ERRORS']['MEMBERS_ERROR']] });
+			  }
+		  } else {
+			  res.status(200).json(teamMembers) ;
+		  }
+		})
+  });
 
   return router
 }

c9d153037cef6234c54e41e46b7a18c0b8ccaf57
Thoai-Vi Le <thoai-vi.le@polymtl.ca>
2019-11-02 21:52:57 -0400

    pushing feed.js, NOT DONE... missing query param

--- TP3/src/routes/rest/feed.js
+++ TP3/src/routes/rest/feed.js
@@ -4,6 +4,25 @@ module.exports = serviceFeed => {
   const router = express.Router()
 
   // À COMPLÉTER
-
+  router.get('/', (req, res, next) => {
+    const language = req.app.locals.lang;
+    const query = { /*from: <DATE>, sort: { field: <string>, order: <ASC|DESC> }*/ }
+		serviceFeed.getSeminars(query)(language)((err, seminars) => {
+			if (err) {
+				if (req.app.locals.t === undefined || req.app.locals.t['ERRORS'] === undefined || req.app.locals.t['ERRORS']['FEEDS_ERROR'] === undefined) {
+					res.status(500).json({
+						'errors': [err.message]
+					});
+				} else {
+					res.status(500).json({
+						'errors': [req.app.locals.t['ERRORS']['FEEDS_ERROR']]
+					});
+				}
+			} else {
+				res.status(200).json(seminars);
+			}
+		})
+  });
+  
   return router
 }

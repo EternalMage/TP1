77896c5b0a4238afcd5e209cb2d534dbbdd6e916
Thoai-Vi Le <thoai-vi.le@polymtl.ca>
2019-11-03 21:51:31 -0500

    push progres template publications.js (pas fini)

--- TP3/src/routes/publications.js
+++ TP3/src/routes/publications.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const moment = require('moment')
+const request = require("request");
+
+const url = "http://localhost:3000/api/publications";
 
 // À COMPLÉTER
+router.get('/', (req, res, next) => {
+    request.get(url, (error, response, body) => {
+        if (error) {
+            throw error
+        } else {
+            let publications = JSON.parse(body);
+            console.log(publications)
+            /*res.render('./../views/team', {
+                members: members
+            }, (err, html) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.send(html);
+                }
+            });*/
+        }
+    });
+});
 
 module.exports = router

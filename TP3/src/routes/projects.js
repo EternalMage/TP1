b7f9cf6ad33a89930a2a38df25ea65d23055ec74
Thoai-Vi Le <thoai-vi.le@polymtl.ca>
2019-11-03 21:42:48 -0500

    definition template project.js et team.js done

--- TP3/src/routes/projects.js
+++ TP3/src/routes/projects.js
@@ -1,6 +1,49 @@
 const express = require('express')
 const router = express.Router()
+const request = require("request");
 
 // À COMPLÉTER
+const url = "http://localhost:3000/api/projects";
+
+router.get('/', (req, res, next) => {
+    request.get(url, (error, response, body) => {
+        if (error) {
+            throw error
+        } else {
+            let projects = JSON.parse(body);
+            res.render('./../views/projects', {
+                projects: projects
+            }, (err, html) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.send(html);
+                }
+            });
+        }
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    request.get(url + '/' + req.params.id, (error, response, body) => {
+        if (error) {
+            throw error
+        } else {
+            let project = JSON.parse(body);
+            console.log(body);
+            let projectPublications = project.publications
+            res.render('./../views/project', {
+                project: project,
+                publications: projectPublications
+            }, (err, html) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.send(html);
+                }
+            });
+        }
+    });
+});
 
 module.exports = router
--- TP3/src/routes/team.js
+++ TP3/src/routes/team.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const request = require("request");
 
 // À COMPLÉTER
+const url = "http://localhost:3000/api/members";
+
+router.get('/', (req, res, next) => {
+    request.get(url, (error, response, body) => {
+        if (error) {
+            throw error
+        } else {
+            let members = JSON.parse(body);
+            res.render('./../views/team', {
+                members: members
+            }, (err, html) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.send(html);
+                }
+            });
+        }
+    });
+});
 
 module.exports = router

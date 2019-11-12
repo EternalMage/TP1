b4e35ebcf86bc526fb0037b6060870eaa0e5ea47
Jonathan G.-R <jonathan.g-r@outlook.com>
2019-11-11 19:51:24 -0500

    Affichage des publications

--- TP3/src/services/mongo/publication.js
+++ TP3/src/services/mongo/publication.js
@@ -16,8 +16,15 @@ const mongodb = require('mongodb')
  *  @param {numPublicationsCallback} callback - Fonction de rappel pour obtenir le résultat
  */
 const getNumberOfPublications = db => callback => {
-  // À COMPLÉTER
-  callback(null, 0)
+    // À COMPLÉTER
+    db.collection('publications').find().toArray((err, content) => {
+        if (err) {
+            callback(err, null)
+        } else {
+            const publications = ((content === null) ? [] : content)
+            callback(null, publications.length)
+        }
+    })
 }
 
 /**
@@ -42,9 +49,52 @@ const getNumberOfPublications = db => callback => {
  *  @param {publicationsCallback} callback - Fonction de rappel pour obtenir le résultat
  */
 const getPublications = db => pagingOpts => callback => {
-  // À COMPLÉTER
-  callback(null, [])
+    db.collection('publications').find().toArray((err, content) => {
+        if (err) {
+            callback(err, null)
+        } else {
+            const publications = ((content === null) ? [] : content)
+                .sort(comparePublications(pagingOpts))
+                .map(publication => {
+                    return {
+                        ...publication,
+                        month: (publication.month === undefined) ? undefined : moment().month(publication.month - 1).format('MMMM')
+                    }
+                })
+
+            if (pagingOpts === undefined || pagingOpts.pageNumber === undefined || pagingOpts.limit === undefined) {
+                callback(null, publications)
+            } else {
+                const startIndex = (pagingOpts.pageNumber - 1) * pagingOpts.limit
+                const endIndex = startIndex + pagingOpts.limit
+                const topNPublications = publications.slice(startIndex, endIndex)
+                callback(null, topNPublications)
+            }
+        }
+    })
 }
+
+/**
+ *  Fonction de comparaison de publications.
+ *
+ *  @param pagingOpts - Options pour la pagination qui contient entre autre
+ *    des options pour le trie
+ *  @param p1 - Première publication à comparer
+ *  @param p2 - Deuxième publication à comparer
+ *  @returns Valeurs de comparaison -1, 1 ou 0
+ */
+const comparePublications = pagingOpts => (p1, p2) => {
+    return pagingOpts.sorting.reduce((acc, sort) => {
+        if (acc === 0) {
+            const field = sort[0] === "date" ? "year" : sort[0]
+            const order = sort[1]
+            const compare = p1[field] < p2[field] ? -1 : p1[field] > p2[field] ? 1 : 0
+            return order === 'asc' ? compare : order === 'desc' ? -compare : compare
+        }
+        return acc
+    }, 0)
+}
+
 
 /**
  * Fonction de rappel pour obtenir la publication créée.
@@ -62,8 +112,8 @@ const getPublications = db => pagingOpts => callback => {
  *  @param {createdPublicationCallback} callback - Fonction de rappel pour obtenir la publication créée
  */
 const createPublication = db => publication => callback => {
-  // À COMPLÉTER
-  callback()
+    // À COMPLÉTER
+    callback()
 }
 
 /**
@@ -74,8 +124,8 @@ const createPublication = db => publication => callback => {
  *  @param callback - Fonction de rappel qui valide la suppression
  */
 const removePublication = db => id => callback => {
-  // À COMPLÉTER
-  callback()
+    // À COMPLÉTER
+    callback()
 }
 
 /**
@@ -94,16 +144,22 @@ const removePublication = db => id => callback => {
  *  @param {projectPublicationsCallback} callback - Fonction de rappel pour obtenir le résultat
  */
 const getPublicationsByIds = db => pubIds => callback => {
-  // À COMPLÉTER
-  callback(null, [])
+    // À COMPLÉTER
+    getPublications(db)(undefined)((err, projects) => {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, publications.filter(publication => pubIds.includes(publication.key)).sort((p1, p2) => p1.year < p2.year ? 1 : -1))
+        }
+    })
 }
 
 module.exports = db => {
-  return {
-    getPublications: getPublications(db),
-    createPublication: createPublication(db),
-    removePublication: removePublication(db),
-    getPublicationsByIds: getPublicationsByIds(db),
-    getNumberOfPublications: getNumberOfPublications(db)
-  }
+    return {
+        getPublications: getPublications(db),
+        createPublication: createPublication(db),
+        removePublication: removePublication(db),
+        getPublicationsByIds: getPublicationsByIds(db),
+        getNumberOfPublications: getNumberOfPublications(db)
+    }
 }
